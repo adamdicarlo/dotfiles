94e86288e9ac6fa28973cd2d5af48c15af781934
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2010-09-17 10:20:08 -0700

    Clobber ~/.gitk, don't special-case drush, it's unnecessary.

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,19 @@
 # Serves as a wrapper to ln.
 #
 deploy_config () {
-  SOURCE=$(pwd)/$1
-  TARGET=$2
-  if [[ -f $TARGET && ! -h $TARGET ]]; then
-    echo "$TARGET exists and is not a symbolic link! Aborting."
-    exit 1
+  SOURCE="$(pwd)/$1"
+  TARGET="$2"
+  if [[ -f "$TARGET" && ! -h "$TARGET" ]]; then
+    # Clobber .gitk if it already exists. gitk always updates it.
+    if [[ "$1" == "gitk" ]]; then
+      echo "Removing ~/.gitk"
+    else
+      echo "$TARGET exists and is not a symbolic link! Aborting."
+      exit 1
+    fi
   fi
-  rm $TARGET 2>/dev/null
-  ln -s $SOURCE $TARGET
+  rm "$TARGET" 2>/dev/null
+  ln -s "$SOURCE" "$TARGET"
 }
 
 #
@@ -21,11 +26,9 @@ deploy_config () {
 main () {
   cd home
   for FILE in *; do
-    deploy_config $FILE ~/.$FILE
+    deploy_config "$FILE" ~/."$FILE"
   done
   cd ..
-
-  deploy_config drush ~/.drush
 }
 
 main
