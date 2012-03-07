294feff375d4cf7b3b19bfb344b41679a203864c
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2012-03-07 14:43:36 -0800

    Add drush aliases for test hosts.

--- home/drush/aliases.drushrc.php
+++ home/drush/aliases.drushrc.php
@@ -1,17 +1,39 @@
 <?php
 
-$my_projects = array(
-  'cfdn',
-  'dr',
-  'ip',
-  'potp',
-  'vr',
-  'wd',
+// Gargravarr test hosts.
+$gargravarr = array(
+  'atka',
+  'bachelor',
+  'copahue',
+  'demo',
+  'dempo',
+  'eldfell',
+  'fuego',
+  'galeras',
+  'hood',
+  'isspah',
+  'jornada',
+  'karthala',
+  'langila',
+  'myoko',
+  'nazko',
+  'okataina',
+  'pinatubo',
+  'qualibou',
+  'rainier',
+  'shasta',
+  'tsurumi',
+  'undara',
+  'visoke',
+  'waiowa',
+  'yellowstone',
+  'zimina',
 );
-foreach ($my_projects as $project) {
-  $aliases[$project] = array(
+foreach ($gargravarr as $g_host) {
+  $aliases[$g_host] = array(
     'uri' => 'default',
-    'root' => '/home/adam/projects/' . $project . '/app/drupal',
+    'root' => '/var/www/' . $g_host . '/current',
+    'remote-user' => $_SERVER['USER'],
+    'remote-host' => 'gargravarr.opensourcery.com',
   );
 }
-
