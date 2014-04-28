af2c74da12a00fde83a264573beee88ec07170ba
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2014-04-27 22:20:58 -0700

    use z with fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,6 +8,11 @@ set fish_theme robbyrussell
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
 
+set -gx Z_SCRIPT_PATH $HOME/bin/tools/z/z.sh
+
+set fish_plugins brew git node rake rbenv vi-mode z
+
+
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
