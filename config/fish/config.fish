a12d226a0e1a31d8d9efcedb0eef9e893ec595f8
Adam DiCarlo <adam.dicarlo@gmail.com>
2014-11-26 09:41:38 -0800

    Many things!

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,6 @@
 # Path to your oh-my-fish.
 set fish_path $HOME/bin/tools/oh-my-fish
+set fish_custom $HOME/dotfiles/config/fish/oh-my-fish-custom
 
 # Theme
 set fish_theme robbyrussell
@@ -10,11 +11,37 @@ set fish_theme robbyrussell
 
 set -gx Z_SCRIPT_PATH $HOME/bin/tools/z/z.sh
 
-set fish_plugins brew git node rake rbenv vi-mode z
-
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+set fish_plugins brew gi node rake vi-mode z
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+. $HOME/.config/fish/private.fish
+
+set -x GIT_EDITOR vim
+set -x EDITOR mvim
+set -x VISUAL mvim
+
+set -x DOCKER_HOST "127.0.0.1:32000"
+set -x NPM_PACKAGES "$HOME/.npm-packages"
+set -x NODE_PATH $NPM_PACKAGES/lib/node_modules $NODE_PATH
+
+function add_path
+  if test -d $argv
+    set PATH $argv $PATH
+  end
+end
+
+add_path "$HOME/bin"
+add_path "/usr/local/bin"
+add_path "/usr/local/sbin"
+add_path "$NPM_PACKAGES/bin"
+add_path "/usr/local/opt/coreutils/libexec/gnubin"
+
+# set -gx RBENV_ROOT /usr/local/var/rbenv
+status --is-interactive; and . (rbenv init -|psub)
+
+# Aliases
+alias ... ../..
+alias .... ../../..
+alias ..... ../../../..
