450ca2fbc8b14d1260c36e9032dc650b8eeaf90a
Adam DiCarlo <adam.dicarlo@gmail.com>
2016-07-26 14:19:00 -0700

    Add karabiner configurator for capslock=ctrl/esc

---
+++ configure-karabiner.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set bilalh.remap.f19_escape_control 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
+$cli set repeat.wait 33
+/bin/echo -n .
+/bin/echo
