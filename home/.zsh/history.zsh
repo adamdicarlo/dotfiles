5153c26b29f852bd1c482d4e012b913a3e3464f4
Adam DiCarlo <97462+adamdicarlo@users.noreply.github.com>
2019-09-26 11:34:22 -0700

    :mag: Fix up/down key history search

---
+++ home/.zsh/history.zsh
@@ -0,0 +1,5 @@
+export HISTFILE=~/.zsh_history
+export HISTSIZE=50000
+export SAVEHIST=50000
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
--- home/.zsh/zplug.zsh
+++ home/.zsh/zplug.zsh
@@ -4,14 +4,26 @@ source $ZPLUG_HOME/init.zsh
 #zplug "djui/alias-tips"
 #zplug "zsh-users/zsh-autosuggestions"
 #zplug "zsh-users/zsh-completions"
-zplug "zsh-users/zsh-syntax-highlighting", defer:2 # Should be loaded 2nd last.
-#zplug "zsh-users/zsh-history-substring-search", defer:3 # Should be loaded last.
-zplug "mafredri/zsh-async", defer:0, from:github
-zplug "sindresorhus/pure", as:theme, from:github, use:pure.zsh
+
 zplug "chriskempson/base16-shell", use:"scripts/base16-dracula.sh", defer:0
+zplug "mafredri/zsh-async", defer:0, from:github
+
+# clipcopy, clippaste commands that work the same on macOS & Linux
+zplug "lib/clipboard", from:oh-my-zsh
+
+zplug "plugins/git", from:oh-my-zsh
+
+zplug "sindresorhus/pure", as:theme, from:github, use:pure.zsh
+
+zplug "zsh-users/zsh-completions"
+zplug "tarruda/zsh-autosuggestions",            defer:3
+zplug "zsh-users/zsh-syntax-highlighting",      defer:3
+zplug "zsh-users/zsh-history-substring-search", defer:3
 
 #zplug "unixorn/tumult.plugin.zsh"
 #zplug "junegunn/fzf", use:"shell/*.zsh"
+
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
 # Check for uninstalled plugins.
 if ! zplug check --verbose; then
